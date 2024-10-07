4b6f760bccbf0f0115439249eb312ac8c3e1aa52
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2024-10-07 22:53:25 +0900

    code : validation check + input onChange 형변환 에러 수정

--- auto-excel-mk2/src/components/BottomBar/index.tsx
+++ auto-excel-mk2/src/components/BottomBar/index.tsx
@@ -7,7 +7,17 @@ const BottomBar = () => {
 
   const onSubmit = () => {
     if (filePath !== "") {
-      window.ipc.send("edit-excel", { path: filePath, data: dataList });
+      const inputData = [];
+
+      for (let data of dataList) {
+        const num = Number(data);
+        if (isNaN(num)) {
+          alert(`숫자가 아닌 값을 입력하여 실행할 수 없습니다! \n해당 값을 수정한 후 다시 실행해주세요. \n(문제되는 값 : ${data})`);
+          return;
+        }
+        inputData.push(num);
+      }
+      window.ipc.send("edit-excel", { path: filePath, data: inputData });
     }
   };
 
